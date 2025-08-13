54d23ecb22c03009c8962480d3b0320c58c3e020
Bapun <mrutyunjayagadanayak@yandex.com>
2025-08-13 09:10:07 +0530

    Stopped the entire file from loading into memory

--- usr/lib/linuxmint/mintbackup/mintbackup.py
+++ usr/lib/linuxmint/mintbackup/mintbackup.py
@@ -23,7 +23,7 @@ import setproctitle
 import secrets
 import base64
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.backends import default_backend
 setproctitle.setproctitle("mintbackup")
@@ -59,7 +59,7 @@ def print_timing(func):
 
 def get_aes_key_from_password(password, salt):
     kdf = PBKDF2HMAC(
-        algorithm=hashlib.sha256(),
+        algorithm=hashes.sha256(),
         length=32,
         salt=salt,
         iterations=100000,
@@ -90,17 +90,21 @@ def decrypt_file_stream(input_path, output_path, password):
     with open(input_path, 'rb') as fin:
         salt = fin.read(16)
         nonce = fin.read(12)
-        file_data = fin.read()
-        tag = file_data[-16:]
-        ciphertext = file_data[:-16]
+        filesize = os.path.getsize(input_path)
+        fin.seek(filesize - 16)
+        tag = fin.read(16)
+        fin.seek(28)
         key = get_aes_key_from_password(password, salt)
         cipher = Cipher(algorithms.AES(key), modes.GCM(nonce, tag), backend=default_backend())
         decryptor = cipher.decryptor()
 
         chunk_size = 64 * 1024
         with open(output_path, 'wb') as fout:
-            for i in range(0, len(ciphertext), chunk_size):
-                fout.write(decryptor.update(ciphertext[i:i+chunk_size]))
+            bytes_remaining = filesize - 28 - 16
+            while bytes_remaining > 0:
+                chunk = fin.read(min(chunk_size, bytes_remaining))
+                fout.write(decryptor.update(chunk))
+                bytes_remaining -= len(chunk)
             fout.write(decryptor.finalize())
 
 class MintBackup:
